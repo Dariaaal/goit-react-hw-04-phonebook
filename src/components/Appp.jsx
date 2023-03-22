533108a64887cd634bbb6bec3d92f374cc9b9220
Dariaaal <1406dariaal@gmail.com>
2023-03-22 18:51:24 +0200

    localStorage

--- src/components/Appp.jsx
+++ src/components/Appp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CardForm from "./phonebook/CardForm";
 import CardList from "./phonebook/CardList";
 import initialContacts from "./phonebook/contacts.json";
@@ -6,12 +6,19 @@ import Filter from "./phonebook/Filter";
 import { nanoid } from "nanoid";
 
 export default function App() {
-    const [contacts, setContacts] = useState(initialContacts);
+    const [contacts, setContacts] = useState(() => {
+      return JSON.parse(window.localStorage.getItem('contacts')) ?? 'initialContacts';
+    });
+     
     const [filter, setFilter] = useState('');
 
     const deleteContact = contactId => {
         setContacts(contact => contact.filter(contact => contact.id !== contactId))
     }
+    
+    useEffect(() => {
+      window.localStorage.setItem('contacts', JSON.stringify(contacts));
+    }, [contacts])
 
     return (
         <div
