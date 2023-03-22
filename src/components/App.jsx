ffdabfff7a5149647c42a5d1badbbd6a86741182
Dariaaal <1406dariaal@gmail.com>
2023-03-23 01:07:43 +0200

    useMemo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import CardForm from "./phonebook/CardForm";
 import CardList from "./phonebook/CardList";
 import initialContacts from "./phonebook/contacts.json";
@@ -60,8 +60,13 @@ export default function App() {
   //     return filteredContacts;
   //  }
 
-   const visibleContacts = contacts.filter(contact => 
-    contact.name.toLowerCase().includes(filter.toLowerCase()));
+  const visibleContacts = useMemo(() => {
+    return contacts.filter(contact => 
+      contact.name.toLowerCase().includes(filter.toLowerCase()));
+  }, [filter, contacts])
+
+  //  const visibleContacts = contacts.filter(contact => 
+  //   contact.name.toLowerCase().includes(filter.toLowerCase()));
 
   //    getVisibleContacts = () => {
   //   const {contacts, filter} = this.state;
